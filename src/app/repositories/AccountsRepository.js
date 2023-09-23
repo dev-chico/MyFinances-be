ec1c58fe81395c4ec954a9b0355ad39d7b10593e
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-09-23 16:39:26 -0300

    feat: add new scripts sql, create accounts repository

---
+++ src/app/repositories/AccountsRepository.js
@@ -0,0 +1,47 @@
+const db = require('../../database');
+
+class AccountsRepository {
+  async findAll() {
+    const rows = await db.query('SELECT * FROM accounts;');
+    return rows;
+  }
+
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM accounts WHERE id = $1;', [id]);
+    return row;
+  }
+
+  async create({ number, balance, userId, investmentId }) {
+    const [row] = await db.query(
+      `
+    INSERT INTO accounts(numero, saldo, fk_userId, fk_investmentId)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+    `,
+      [number, balance, userId, investmentId]
+    );
+
+    return row;
+  }
+
+  async update(id, { number, balance, userId, investmentId }) {
+    const [row] = await db.query(
+      `
+      UPDATE accounts
+      SET numero = $1, saldo = $2, fk_userId = $3, fk_investmentId = $4
+      WHERE id = $5
+      RETURNING *;
+      `,
+      [number, balance, userId, investmentId, id]
+    );
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM accounts WHERE id = $1;', [id]);
+    return deleteOp;
+  }
+}
+
+module.exports = new AccountsRepository();
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -9,3 +9,38 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR NOT NULL,
   email VARCHAR UNIQUE NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS investments (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  investmentType VARCHAR(20) NOT NULL,
+  rescue_time INTEGER NOT NULL,
+  yield NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  numero VARCHAR(10) UNIQUE NOT NULL,
+  saldo NUMERIC NOT NULL,
+  fk_userId UUID,
+  fk_investmentId UUID,
+  FOREIGN KEY (fk_userId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_investmentId) REFERENCES investments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS movements (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  movementType VARCHAR(100),
+  movementDate DATE,
+  movementValue NUMERIC,
+  fk_accountId UUID,
+  FOREIGN KEY (fk_accountId) REFERENCES accounts (id) ON DELETE CASCADE
+);
+
+INSERT INTO investments (investmentType, rescue_time, yield)
+VALUES
+('Tesouro Pré-Fixado', 60, 102),
+('Tesouro Selic', 120, 105),
+('Tesouro IPCA+', 180, 108),
+('CDB & LC', 210, 111),
+('LCI & LCA', 240, 115);
